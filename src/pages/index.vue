6bc999dbe04761513f0d5a099ec19f7f6b213a03
Rakesh Jangid <rakesh@jangid.co.in>
2024-04-09 12:41:38 +0530

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+	import { useStore } from '@/store';
+	import maa from '@/assets/maa-gss.jpg';
+	useHead({
+		title: 'GSS Girls PG College, Chirawa',
+	});
+
+	const store = useStore();
+</script>
+
+<template>
+	<div class="grid place-items-center min-h-[80vh]">
+		<div>
+			<img :src="maa" width="300" class="mx-auto" />
+			<h2 class="py-5 text-3xl my-3">GSS Girls (PG) College, Chirawa</h2>
+		</div>
+	</div>
+</template>
+
+<route lang="yaml">
+name: home
+</route>
